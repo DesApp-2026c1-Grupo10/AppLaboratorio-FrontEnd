7bd83aaaa90af9222522b0b6c95858044aff7447
NahuelNC1 <nahuel.negreticarballo@estudiantes.unahur.edu.ar>
2026-05-20 19:50:54 -0300

    mejoras visuales en el boton de cerrar sesion

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -156,7 +156,7 @@ export default function Sidebar() {
 
           <Button
             fullWidth
-            variant="contained"
+            variant="outlined" //antes "contained"
 
             className="sidebar-logout-btn"
 
